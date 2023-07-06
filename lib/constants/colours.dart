70e32006e725b3acce018277305fe66afd073a11
Arya Jha <jhaarya1002@gmail.com>
2023-07-06 19:06:09 +0530

    Final

---
+++ lib/constants/colours.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Colors.pinkAccent;
+const Color tdBlue = Colors.blueAccent;
+
+const Color tdBlack = Colors.black54;
+const Color tdGrey = Colors.blueGrey;
+
+const Color tdBgcolor = Colors.white;
